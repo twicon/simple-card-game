50f9b3cd9775d08f5043a7d4497b596b3cfb4138
Marcus Grandien <marcus.grandien@gmail.com>
2023-12-07 16:36:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+let player1Score = 0;
+let player2Score = 0;
+let currentPlayer = 1;
+
+const cardObjects = [
+    { id: 'A1', pair: 'A', matched: false },
+    { id: 'A2', pair: 'A', matched: false },
+    { id: 'B1', pair: 'B', matched: false },
+    { id: 'B2', pair: 'B', matched: false },
+    { id: 'C1', pair: 'C', matched: false },
+    { id: 'C2', pair: 'C', matched: false },
+    { id: 'D1', pair: 'D', matched: false },
+    { id: 'D2', pair: 'D', matched: false },
+    { id: 'E1', pair: 'E', matched: false },
+    { id: 'E2', pair: 'E', matched: false },
+    { id: 'F1', pair: 'F', matched: false },
+    { id: 'F2', pair: 'F', matched: false },
+    { id: 'G1', pair: 'G', matched: false },
+    { id: 'G2', pair: 'G', matched: false },
+    { id: 'H1', pair: 'H', matched: false },
+    { id: 'H2', pair: 'H', matched: false },
+];
+
+// Shuffle the cardObjects
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+function initGame() {
+    shuffle(cardObjects);
+    const gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = ''; // Clear the board
+
+    for (let card of cardObjects) {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.setAttribute('data-id', card.id);
+
+        cardElement.addEventListener('click', function() {
+            flipCard(card, cardElement);
+        });
+
+        gameBoard.appendChild(cardElement);
+    }
+}
+
+let firstCard = null;
+let firstCardElement = null;
+let lockBoard = false; // Used to lock the board during checks
+
+function flipCard(card, cardElement) {
+    if (lockBoard || card.matched || cardElement === firstCardElement) {
+        return; // Ignore clicks if the board is locked, card is already matched, or it's the same card
+    }
+
+    cardElement.textContent = card.pair; // Show card value
+
+    if (!firstCard) {
+        firstCard = card;
+        firstCardElement = cardElement;
+    } else {
+        checkForMatch(card, cardElement);
+    }
+}
+
+function checkForMatch(secondCard, secondCardElement) {
+    lockBoard = true; // Lock the board to prevent further flips
+
+    if (firstCard.pair === secondCard.pair) {
+        // It's a match
+        firstCard.matched = secondCard.matched = true;
+        updateScore();
+        resetBoard();
+    } else {
+        // No match, hide cards again after a short delay
+        setTimeout(() => {
+            firstCardElement.textContent = '';
+            secondCardElement.textContent = '';
+            resetBoard();
+        }, 1000);
+    }
+}
+
+function updateScore() {
+    if (currentPlayer === 1) {
+        player1Score++;
+        document.getElementById('player1Score').textContent = player1Score;
+    } else {
+        player2Score++;
+        document.getElementById('player2Score').textContent = player2Score;
+    }
+}
+
+function updateTurn() {
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+}
+
+function resetBoard() {
+    // Reset board and card variables
+    [firstCard, firstCardElement] = [null, null];
+    lockBoard = false;
+}
+
+initGame();

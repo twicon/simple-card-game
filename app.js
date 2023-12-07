0bbca52c7ad21c25f889c695e3b8d2e0e7c9736c
Marcus Grandien <marcus.grandien@gmail.com>
2023-12-07 17:13:33 +0100

    add pics n logic

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ let player2Score = 0;
 let currentPlayer = 1;
 
 const cardObjects = [
-    { id: 'A1', pair: 'A', matched: false },
-    { id: 'A2', pair: 'A', matched: false },
-    { id: 'B1', pair: 'B', matched: false },
-    { id: 'B2', pair: 'B', matched: false },
-    { id: 'C1', pair: 'C', matched: false },
-    { id: 'C2', pair: 'C', matched: false },
-    { id: 'D1', pair: 'D', matched: false },
-    { id: 'D2', pair: 'D', matched: false },
-    { id: 'E1', pair: 'E', matched: false },
-    { id: 'E2', pair: 'E', matched: false },
-    { id: 'F1', pair: 'F', matched: false },
-    { id: 'F2', pair: 'F', matched: false },
-    { id: 'G1', pair: 'G', matched: false },
-    { id: 'G2', pair: 'G', matched: false },
-    { id: 'H1', pair: 'H', matched: false },
-    { id: 'H2', pair: 'H', matched: false },
+    { id: 'A1', pair: 'A', matched: false, image: 'images/bild1.webp'},
+    { id: 'A2', pair: 'A', matched: false, image: 'images/bild1.webp'},
+    { id: 'B1', pair: 'B', matched: false, image: 'images/bild2.webp' },
+    { id: 'B2', pair: 'B', matched: false, image: 'images/bild2.webp' },
+    { id: 'C1', pair: 'C', matched: false, image: 'images/bild3.webp' },
+    { id: 'C2', pair: 'C', matched: false, image: 'images/bild3.webp' },
+    { id: 'D1', pair: 'D', matched: false, image: 'images/bild4.webp' },
+    { id: 'D2', pair: 'D', matched: false, image: 'images/bild4.webp' },
+    { id: 'E1', pair: 'E', matched: false, image: 'images/bild5.webp' },
+    { id: 'E2', pair: 'E', matched: false, image: 'images/bild5.webp' },
+    { id: 'F1', pair: 'F', matched: false, image: 'images/bild6.webp' },
+    { id: 'F2', pair: 'F', matched: false, image: 'images/bild6.webp' },
+    { id: 'G1', pair: 'G', matched: false, image: 'images/bild7.webp' },
+    { id: 'G2', pair: 'G', matched: false, image: 'images/bild7.webp' },
+    { id: 'H1', pair: 'H', matched: false, image: 'images/bild8.webp' },
+    { id: 'H2', pair: 'H', matched: false, image: 'images/bild8.webp' }
 ];
 
 function shuffle(array) {
@@ -32,18 +32,25 @@ function initGame() {
     updateScores();
 
     shuffle(cardObjects);
-    cardObjects.forEach(card => card.matched = false); // Reset match status
+    cardObjects.forEach(card => card.matched = false);
 
     const gameBoard = document.getElementById('gameBoard');
-    gameBoard.innerHTML = ''; // Clear the board
+    gameBoard.innerHTML = '';
 
     for (let card of cardObjects) {
         const cardElement = document.createElement('div');
         cardElement.classList.add('card');
         cardElement.setAttribute('data-id', card.id);
 
+        const imgElement = document.createElement('img');
+        imgElement.src = card.image; // Corrected image path
+        imgElement.style.display = 'none';
+        imgElement.classList.add('card-image');
+
+        cardElement.appendChild(imgElement);
+
         cardElement.addEventListener('click', function() {
-            flipCard(card, cardElement);
+            flipCard(card, cardElement, imgElement);
         });
 
         gameBoard.appendChild(cardElement);
@@ -54,33 +61,33 @@ let firstCard = null;
 let firstCardElement = null;
 let lockBoard = false;
 
-function flipCard(card, cardElement) {
+function flipCard(card, cardElement, imgElement) {
     if (lockBoard || card.matched || cardElement === firstCardElement) {
         return;
     }
 
-    cardElement.textContent = card.pair;
+    imgElement.style.display = 'block';
 
     if (!firstCard) {
         firstCard = card;
         firstCardElement = cardElement;
     } else {
-        checkForMatch(card, cardElement);
+        checkForMatch(card, cardElement, imgElement);
     }
 }
 
-function checkForMatch(secondCard, secondCardElement) {
+function checkForMatch(secondCard, secondCardElement, secondImgElement) {
     lockBoard = true;
 
     if (firstCard.pair === secondCard.pair) {
         firstCard.matched = secondCard.matched = true;
         updateScore(true);
         resetBoard();
-        checkGameOver(); // Check if game is over after each match
+        checkGameOver();
     } else {
         setTimeout(() => {
-            firstCardElement.textContent = '';
-            secondCardElement.textContent = '';
+            firstCardElement.firstChild.style.display = 'none';
+            secondImgElement.style.display = 'none';
             updateScore(false);
             resetBoard();
         }, 1000);
@@ -114,7 +121,7 @@ function checkGameOver() {
     if (cardObjects.every(card => card.matched)) {
         const winner = player1Score === player2Score ? 'Tie game!' : player1Score > player2Score ? 'Player 1 wins!' : 'Player 2 wins!';
         alert(winner + ' Click OK to restart.');
-        initGame(); // Restart the game
+        initGame();
     }
 }
 
